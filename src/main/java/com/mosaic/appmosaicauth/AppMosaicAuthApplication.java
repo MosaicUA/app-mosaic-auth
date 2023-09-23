425d7c8e5a0371522b1a4ff37e5d74226bcbb327
richn <rolik.alexx@gmail.com>
2023-09-24 00:16:07 +0300

    init repository

---
+++ src/main/java/com/mosaic/appmosaicauth/AppMosaicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mosaic.appmosaicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMosaicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMosaicAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mosaic/appmosaicauth/AppMosaicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosaic.appmosaicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMosaicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
